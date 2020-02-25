8050c04d45310d21857e7dc54a6768314312ed5b
asijaw <asija.w@yahoo.com>
2020-02-25 09:58:51 -0600

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,5 +47,5 @@ def select_character_names_and_number_of_books_they_are_in
     INNER JOIN characters 
       ON character_books.character_id = characters.id 
     GROUP BY characters.name 
-    ORDER BY book_count DESC"
+    ORDER BY book_count DESC, characters.name ASC"
 end
